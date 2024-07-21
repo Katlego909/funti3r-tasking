f343d68b703e234f47334f8c8a138d9e5393ca04
Katlego909 <katlego.hlangwane@gmail.com>
2024-07-21 15:50:01 +0200

    Update to requirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -137,24 +137,24 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'funti3r',
-        'USER': 'postgres',
-        'PASSWORD': 'khanya',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'funti3r',
+#         'USER': 'postgres',
+#         'PASSWORD': 'khanya',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
